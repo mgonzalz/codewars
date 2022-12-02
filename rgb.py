4e21cbe7de5c48c653e086a8de922ee55b66c198
mgonzalz <mgonzgarc@myuax.com>
2022-12-02 21:49:57 +0100

    rgb

--- rgb.py
+++ rgb.py
@@ -1,1 +1,9 @@
+def formula(rgb):
+    if rgb < 0:
+        return 0
+    if rgb > 255:
+        return 255
+    return rgb
 
+def rgb(r, g, b): #0<=rgb<=255
+    return "{:02X}{:02X}{:02X}".format(formula(r), formula(g), formula(b)) #Formato hexadecimal
