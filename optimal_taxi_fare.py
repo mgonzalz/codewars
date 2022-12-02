532050548ec33a836cb30eb325c67d99cc5becb7
mgonzalz <mgonzgarc@myuax.com>
2022-12-02 22:43:07 +0100

    optimal_taxi

--- optimal_taxi_fare.py
+++ optimal_taxi_fare.py
@@ -1,1 +1,9 @@
+def calculate_optimal_fare(d, t, v1, r, v2): #dividido por 60 para pasarlo a minutos
+    kmmin = (d/t)
+    if kmmin<=v2/60:
+        return "0.00"
+    elif kmmin > v1/60:
+        return "Won't make it!"
+    
+    return f"{r*(v1/60)*(d-(v2/60)*t)/((v1/60)-v2/60):.2f}"
 
